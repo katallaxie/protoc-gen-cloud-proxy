144b7585f403006c352c558d22f19f3caca14fb8
Sebastian Döll <sebastian@katallaxie.me>
2020-11-27 16:06:28 +0100

    wip: use go generate

---
+++ gen.go
@@ -0,0 +1,8 @@
+package main
+
+//go:generate mkdir -p api
+//go:generate protoc --go_out=plugins=grpc:./api --go_opt=paths=source_relative --proto_path=proto proto/annotations.proto
+//go:generate protoc --go_out=plugins=grpc:./api --go_opt=paths=source_relative --proto_path=proto proto/empty.proto
+//go:generate protoc --go_out=plugins=grpc:./api --go_opt=paths=source_relative --proto_path=proto proto/fields.proto
+//go:generate protoc --go_out=plugins=grpc:./api --go_opt=paths=source_relative --proto_path=proto proto/methods.proto
+//go:generate protoc --go_out=plugins=grpc:./api --go_opt=paths=source_relative --proto_path=proto proto/sqs.proto
