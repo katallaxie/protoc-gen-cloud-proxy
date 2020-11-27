76f1e17aae2fcc2152e3ca9d148d4034d17666e8
Sebastian Döll <sebastian@katallaxie.me>
2020-11-27 16:27:56 +0100

    wip: use go generate

--- gen.go
+++ gen.go
@@ -6,3 +6,4 @@ package main
 //go:generate protoc --go_out=plugins=grpc:./api --go_opt=paths=source_relative --proto_path=proto proto/fields.proto
 //go:generate protoc --go_out=plugins=grpc:./api --go_opt=paths=source_relative --proto_path=proto proto/methods.proto
 //go:generate protoc --go_out=plugins=grpc:./api --go_opt=paths=source_relative --proto_path=proto proto/sqs.proto
+//go:generate go run -tags codegen internal/gen/proto/main.go -path=pkg/aws aws/apis/*/*/api-2.json
